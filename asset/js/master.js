b6c9c0ab308762f66ce3ffa7f7cfe63592db3a5f
Mehrab <mmehrab.pk@gmail.com>
2025-05-28 17:02:59 +0330

    search box, js part added

--- asset/js/master.js
+++ asset/js/master.js
@@ -19,3 +19,17 @@ closeBurger.addEventListener('click', () => {
     burger.style.right = '-100%'
     bodyOff.style.display = 'none'
 })
+// ----------------------------search box----------------------------------
+let openSearch = document.getElementById('open-search')
+let closeSearch = document.getElementById('close-search')
+let searchBox = document.getElementById('search-box')
+// --------open---------
+openSearch.addEventListener('click' , ()=>{
+    searchBox.style.transform = 'scaleY(1)'
+    bodyOff.style.display = 'flex'
+})
+// --------close---------
+closeSearch.addEventListener('click' , ()=>{
+    searchBox.style.transform = 'scaleY(0)'
+    bodyOff.style.display = 'none'
+})
