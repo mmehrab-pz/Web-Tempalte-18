a42abc7b6a4ead3d0c1cb93fdbd372f60e9afa85
Mehrab <mmehrab.pk@gmail.com>
2025-05-28 15:07:50 +0330

    bergur menu js codes updated

--- asset/js/master.js
+++ asset/js/master.js
@@ -1,17 +1,21 @@
-// ----------------------burger menu-------------------------
+// ----------------------------burger menu----------------------------------
 let openBurger = document.getElementById('open-burger')
 let burger = document.getElementById('burger-menu')
 let closeBurger = document.getElementById('close-burger')
 let mobileBurger = document.getElementById('open-burger-mobile')
+let bodyOff = document.getElementById('bodyOff')
 // --------open---------
-openBurger.addEventListener('click' , ()=>{
+openBurger.addEventListener('click', () => {
     burger.style.right = '0%'
+    bodyOff.style.display = 'flex'
 })
 // --------open mobile---------
-mobileBurger.addEventListener('click' , ()=>{
+mobileBurger.addEventListener('click', () => {
     burger.style.right = '0%'
+    bodyOff.style.display = 'flex'
 })
 // --------close---------
-closeBurger.addEventListener('click' , ()=>{
+closeBurger.addEventListener('click', () => {
     burger.style.right = '-100%'
+    bodyOff.style.display = 'none'
 })
