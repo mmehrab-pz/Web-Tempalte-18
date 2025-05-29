30607a20a50f7b25591fa19f89b3b8af0c7b3afc
Mehrab <mmehrab.pk@gmail.com>
2025-05-29 08:30:11 +0330

    nav bar 2 js movement codes added

--- asset/js/master.js
+++ asset/js/master.js
@@ -24,12 +24,28 @@ let openSearch = document.getElementById('open-search')
 let closeSearch = document.getElementById('close-search')
 let searchBox = document.getElementById('search-box')
 // --------open---------
-openSearch.addEventListener('click' , ()=>{
+openSearch.addEventListener('click', () => {
     searchBox.style.transform = 'scaleY(1)'
     bodyOff.style.display = 'flex'
 })
 // --------close---------
-closeSearch.addEventListener('click' , ()=>{
+closeSearch.addEventListener('click', () => {
     searchBox.style.transform = 'scaleY(0)'
     bodyOff.style.display = 'none'
+})
+// ----------------------------nav bar 2----------------------------------
+let navBar = document.getElementById('headerNav')
+let navBar2 = document.getElementById('headerNav2')
+
+window.addEventListener('scroll', () => {
+    const scrollTop = window.scrollY
+    console.log(scrollTop);
+
+    if (scrollTop == 0) {
+        navBar.style.opacity = '1';
+        navBar2.style.top = '-85px';
+    } else {
+        navBar.style.opacity = '0';
+        navBar2.style.top = '0px';
+    }
 })
